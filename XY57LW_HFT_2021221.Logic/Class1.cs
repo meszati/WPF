58481220d83cf157265675bac15b79f87c75bc7b
Gyéresi Norbert <Gyresi Norbert@DESKTOP-5EORIV9>
2021-10-09 20:30:20 +0200

    Add project files.

---
+++ XY57LW_HFT_2021221.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace XY57LW_HFT_2021221.Logic
+{
+    public class Class1
+    {
+    }
+}
---
+++ XY57LW_HFT_2021221.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace XY57LW_HFT_2021221.Repository
+{
+    public class Class1
+    {
+    }
+}
