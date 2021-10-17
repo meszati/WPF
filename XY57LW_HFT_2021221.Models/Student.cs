7282dfc569896bbb37f80c812923024cea462f52
Norbert-Gyeresi <gyeresinorbi@gmail.com>
2021-10-17 17:57:34 +0200

    Student class created

--- XY57LW_HFT_2021221.Models/Student.cs
+++ XY57LW_HFT_2021221.Models/Student.cs
@@ -1,25 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace XY57LW_HFT_2021221.Models
 {
+    [Table("students")]
     class Student
     {
-        public int ID { get; set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
 
+        public int StudentID { get; set; }
+
+        [MaxLength(40)]
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         public DateTime BirthDate { get; set; }
 
+        [NotMapped]
         public string MothersName { get; set; }
 
+        [Required]
         public string City { get; set; }
 
+        [ForeignKey(nameof(School))]
         public int SchoolOM { get; set; }
 
+        [ForeignKey(nameof(Measurement))]
         public int NetfitID { get; set; }
     }
 }
