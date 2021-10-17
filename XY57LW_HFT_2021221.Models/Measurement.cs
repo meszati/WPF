341a3a2a7274c3c61ab32b516afdfd24f3146a0e
Norbert-Gyeresi <gyeresinorbi@gmail.com>
2021-10-17 18:00:50 +0200

    Measurement class created

--- XY57LW_HFT_2021221.Models/Measurement.cs
+++ XY57LW_HFT_2021221.Models/Measurement.cs
@@ -1,23 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace XY57LW_HFT_2021221.Models
 {
+    [Table("measurements")]
     class Measurement
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
 
+        [Required]
         public double BMI { get; set; }
 
+        [Required]
         public int Pushup { get; set; }
 
+        [Required]
         public int Situp { get; set; }
 
+        [Required]
         public int Jump { get; set; }
 
+        [Required]
         public double Bodyfat { get; set; }
 
 
