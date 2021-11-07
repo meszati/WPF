4d1ab7dca3fccb54145736f4d74a25efaf279ce7
Norbert-Gyeresi <gyeresinorbi@gmail.com>
2021-11-07 21:19:35 +0100

    SchoolRepository class created

--- XY57LW_HFT_2021221.Data/StudentNetfitDbContext.cs
+++ XY57LW_HFT_2021221.Data/StudentNetfitDbContext.cs
@@ -8,7 +8,7 @@ using XY57LW_HFT_2021221.Models;
 
 namespace XY57LW_HFT_2021221.Data
 {
-    class StudentNetfitDbContext : DbContext
+    public class StudentNetfitDbContext : DbContext
     {
         public DbSet<School> Schools { get; set; }
 
---
+++ XY57LW_HFT_2021221.Repository/SchoolRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XY57LW_HFT_2021221.Data;
+using XY57LW_HFT_2021221.Models;
+
+namespace XY57LW_HFT_2021221.Repository
+{
+    public class SchoolRepository : ISchoolRepository
+    {
+        StudentNetfitDbContext ctx;
+
+        public SchoolRepository(StudentNetfitDbContext ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        public void Create(School school)
+        {
+            ctx.Schools.Add(school);
+            ctx.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            ctx.Remove(Read(id));
+            ctx.SaveChanges();
+
+        }
+
+        public School Read(int id)
+        {
+            return ctx.Schools.FirstOrDefault(x => x.SchID == id);
+        }
+
+        public IQueryable<School> ReadAll()
+        {
+            return ctx.Schools;
+        }
+
+        public void Update(School school)
+        {
+            var oldschool = Read(school.SchID);
+            oldschool.Headmaster = school.Headmaster;
+            oldschool.Location = school.Location;
+            oldschool.Name = school.Name;
+            oldschool.Phone = school.Phone;
+            oldschool.Students = school.Students;
+            ctx.SaveChanges();
+        }
+    }
+}
